392f0bc24b8f6a4ce53f6ea7e7eafe52e704a8f2
Helder Silva <helder.dds@gmail.com>
2020-01-22 07:45:42 -0400

    Config tests with Jest and Enzyme

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,22 @@
 /**
  * @format
  */
-
-import 'react-native';
+//import 'react-native';
 import React from 'react';
-import App from '../App';
+import App from '../src/App';
+import {mount, shallow} from 'enzyme';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+// This test just uses Jest snapshot testing
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+// Using Jest + Enzyme
+describe('<App />', () => {
+  it('renders correctly, test using Jest + Enzyme', () => {
+    expect(shallow(<App />)).toMatchSnapshot();
+  });
 });
